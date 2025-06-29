c6b4881f5e0af57e4db478865736df1bbead8711
KRiano314 <kelseyriano8@gmail.com>
2025-06-29 22:43:57 +0800

    Riano: version 6.6 Commit

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,73 +1,21 @@
 import './App.css';
-import { useNavigate, useLocation } from "react-router-dom";
 
-function HrefButtons({ scrollToProjects, scrollToAbout, scrollToResume, scrollToTop, scrollToContact }) {
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  const handleHomeClick = () => {
-    if (location.pathname === "/") {
-      scrollToTop();
-    } else {
-      navigate("/");
-      setTimeout(() => {
-        scrollToTop();
-      }, 100);
-    }
-  };
-
-  const handleProjectsClick = () => {
-    if (location.pathname === "/") {
-      scrollToProjects();
-    } else {
-      navigate("/");
-      setTimeout(() => {
-        scrollToProjects();
-      }, 100);
-    }
-  };
-
-  const handleAboutClick = () => {
-    if (location.pathname === "/") {
-      scrollToAbout();
-    } else {
-      navigate("/");
-      setTimeout(() => {
-        scrollToAbout();
-      }, 100);
-    }
-  };
-
-  const handleResumeClick = () => {
-    if (location.pathname === "/") {
-      scrollToResume();
-    } else {
-      navigate("/");
-      setTimeout(() => {
-        scrollToResume();
-      }, 100);
-    }
-  };
-
-  const handleContactsClick = () => {
-    if (location.pathname === "/") {
-      scrollToContact();
-    } else {
-      navigate("/");
-      setTimeout(() => {
-        scrollToContact();
-      }, 100);
-    }
-  };
+function HrefButtons({ 
+  scrollToProjects, 
+  scrollToAbout, 
+  scrollToResume, 
+  scrollToTop, 
+  scrollToContact 
+}) {
 
   return (
     <nav className="Navbar">
       <ul>
-        <button onClick={handleHomeClick}>Home</button>
-        <button onClick={handleAboutClick}>About Me</button>
-        <button onClick={handleProjectsClick}>Projects</button>
-        <button onClick={handleResumeClick}>Resume</button>
-        <button onClick={handleContactsClick}>Contact Me</button>
+        <button onClick={scrollToTop}>Home</button>
+        <button onClick={scrollToAbout}>About Me</button>
+        <button onClick={scrollToProjects}>Projects</button>
+        <button onClick={scrollToResume}>Resume</button>
+        <button onClick={scrollToContact}>Contact Me</button>
       </ul>
     </nav>
   );
